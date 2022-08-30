94507b0e25622cca9b06ff1de3f190058206d75a
Ritesh Shergill <riteshshergill@gmail.com>
2022-08-30 19:45:32 +0530

    Added toyssection view

---
+++ src/components/ToysSection.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import '../styles/ToysSection.css';
+
+//rfce to create boilerplate
+
+function ToysSection() {
+
+  return (
+    <>
+    <div className="content margin-top">
+        <div class="row">
+            <div className="toysSection bg-cover text-white col-sm">
+            
+            
+                <img className="pl-5" src="https://m.media-amazon.com/images/I/81SkdtodFuL._SY550_.jpg"/>
+                <img className="pl-5" src="https://m.media-amazon.com/images/M/MV5BMTE5OTM0Njc4NF5BMl5BanBnXkFtZTcwMjg4NjIzMQ@@._V1_.jpg"/>
+                <img className="pl-5" src="https://m.media-amazon.com/images/I/71P2xntEewL._SX522_.jpg"/>
+                <img className="pl-5" src="https://m.media-amazon.com/images/M/MV5BYThhMDE0NjEtZjA5NS00YjlkLWI0ZWEtOTViMmZhOGNjMGU5XkEyXkFqcGdeQXVyNTU2NDQ3NjU@._V1_.jpg"/>
+                <img className="pl-5" src="https://www.beingtheparent.com/wp-content/uploads/2017/06/Lego-good-or-bad.jpg"/>
+            
+            </div>
+        </div>
+        <div class="row margin-row ">
+            <div className="toysSection bg-cover text-white col-sm">
+            
+            
+                <img className="pl-5" src="https://m.media-amazon.com/images/M/MV5BMTMyNzA3ODA0OF5BMl5BanBnXkFtZTcwNTc5NTEzMQ@@._V1_.jpg"/>
+                <img className="pl-5" src="https://i.pinimg.com/originals/a9/0d/bc/a90dbcafba774eed200c8515cc477d7a.jpg"/>
+                <img className="pl-5" src="https://www.forbesindia.com/media/images/2022/Apr/img_181549_goldspot_bg.jpg"/>
+                <img className="pl-5" src="https://i.insider.com/560ebbe7dd0895325c8b458e?width=1039"/>
+                <img className="pl-5" src="https://5.imimg.com/data5/XD/VT/MY-4620137/melody-chocolate-500x500.png"/>
+            
+            </div>
+        </div>    
+    
+    </div>
+      
+      
+       
+
+    </>
+    
+    
+  )
+}
+
+export default ToysSection
